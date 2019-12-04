c4e9097abfa0258ed4b0e9e9823b3c173eef8118
yueyanbin <yyb_home@126.com>
2019-12-04 11:57:30 +0800

    初始化

--- page.config.js
+++ page.config.js
@@ -1,73 +1,70 @@
 /* eslint-disable */
 module.exports = [
+  // {
+  //   name: './login.html',
+  //   html: 'login/login.html',
+  //   jsEntry: 'login/login.js'
+  // },
+  // {
+  //   name: 'rule',
+  //   html: 'rule/rule.html',
+  //   jsEntry: 'rule/rule.js'
+  // },
+  // {
+  //   name: 'polyfill',
+  //   html: 'polyfill/polyfill.html',
+  //   jsEntry: 'polyfill/polyfill.js'
+  // },
+  // {
+  //   name: 'difference',
+  //   html: 'difference/difference.html',
+  //   jsEntry: 'difference/difference.js'
+  // },
+  // {
+  //   name: 'freeassessment',
+  //   html: 'freeassessment/freeassessment.html',
+  //   jsEntry: 'freeassessment/freeassessment.js'
+  // },
+  // {
+  //   name: 'parents',
+  //   html: 'parents/parents.html',
+  //   jsEntry: 'parents/parents.js'
+  // },
+  // {
+  //   name: 'login',
+  //   html: 'login/login.html',
+  //   jsEntry: 'login/login.js'
+  // },
+  // {
+  //   name: 'teacher',
+  //   html: 'teacher/teacher.html',
+  //   jsEntry: 'teacher/teacher.js'
+  // },
+  // {
+  //   name: 'studentexp',
+  //   html: 'studentexp/studentexp.html',
+  //   jsEntry: 'studentexp/studentexp.js'
+  // },
+  // {
+  //   name: 'methodology',
+  //   html: 'methodology/methodology.html',
+  //   jsEntry: 'methodology/methodology.js'
+  // },
+  // {
+  //   name: 'referfriend',
+  //   html: 'referfriend/referfriend.html',
+  //   jsEntry: 'referfriend/referfriend.js'
+  // }, 
   {
-    name: './login.html',
-    html: 'login/login.html',
-    jsEntry: 'login/login.js'
-  },
-  {
-    name: 'rule',
-    html: 'rule/rule.html',
-    jsEntry: 'rule/rule.js'
-  },
-  {
-    name: 'contact',
-    html: 'contact/contact.html',
-    jsEntry: 'contact/contact.js'
-  },
-  {
-    name: 'polyfill',
-    html: 'polyfill/polyfill.html',
-    jsEntry: 'polyfill/polyfill.js'
-  },
-  {
-    name: 'difference',
-    html: 'difference/difference.html',
-    jsEntry: 'difference/difference.js'
-  },
-  {
-    name: 'freeassessment',
-    html: 'freeassessment/freeassessment.html',
-    jsEntry: 'freeassessment/freeassessment.js'
-  },
-  {
-    name: 'parents',
-    html: 'parents/parents.html',
-    jsEntry: 'parents/parents.js'
-  },
-  {
-    name: 'login',
-    html: 'login/login.html',
-    jsEntry: 'login/login.js'
-  },
-  {
-    name: 'teacher',
-    html: 'teacher/teacher.html',
-    jsEntry: 'teacher/teacher.js'
-  },
-  {
-    name: 'studentexp',
-    html: 'studentexp/studentexp.html',
-    jsEntry: 'studentexp/studentexp.js'
-  },
-  {
-    name: 'methodology',
-    html: 'methodology/methodology.html',
-    jsEntry: 'methodology/methodology.js'
-  },
-  {
-    name: 'referfriend',
-    html: 'referfriend/referfriend.html',
-    jsEntry: 'referfriend/referfriend.js'
-  }, {
     name: 'index',
     html: 'index/index.html',
     jsEntry: 'index/index.js'
-  }, {
-    name: 'contactus',
-    html: 'contactus/contactus.html',
-    jsEntry: 'contactus/contactus.js'
   }
+  // , {
+  //   name: 'contactus',
+  //   html: 'contactus/contactus.html',
+  //   jsEntry: 'contactus/contactus.js'
+  // }
 
 
 
--- src/common/js/footer.js
+++ src/common/js/footer.js
@@ -12,28 +12,6 @@
     trustpilotEle.innerHTML = trustpilot;
     document.body.appendChild(trustpilotEle);
 
-    var html = `
-        <div class="lg-icon lg-logo"></div>
-        <p class="info">
-            LingoAce is the premiere provider of online Chinese classes
-            for students aged 6-15. Our mission is to help make the world
-            a better place through the joy of language learning.
-        </p>
-        <ul class="icon-warpper">
-            <li><a href="https://apps.apple.com/cn/app/lingoace/id1155911183"><i class="lg-icon lg-icon-appstore"></i></a></li>
-            <li><a href="http://www.kidsafeseal.com/certifiedproducts/lingoace.html"><i class="lg-icon lg-icon-kidsafe"></i></a></i></li>
-            <li></li>
-        </ul>
-        <ul class="footer-menus">
-            <li><a href="./contactus.html"><i></i> Contact Us</a></li>
-            <li><a href="./rule.html"><i></i> Terms &amp; Conditions</a></li>
-        </ul>
-        <p class="copy">Copyright © 2019 LingoAce</p>`;
-
-    var footer = document.createElement('footer');
-    footer.innerHTML = html;
-    document.body.appendChild(footer);
-
     var script = document.createElement("script");
     script.type = "text/javascript";
     script.src = "//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js";
